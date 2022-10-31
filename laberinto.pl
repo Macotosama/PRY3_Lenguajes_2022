0b10b02ce3ffe76ad948f6afbdd0bdc059e07955
tim-scarlith <anthony00052023@gmail.com>
2022-10-30 20:19:02 -0600

    commit

--- laberinto.pl
+++ laberinto.pl
@@ -25,6 +25,65 @@ accionBasadaEnEstado(left, at).
 
 
 /**
+*contador encargado 
+*/
+contador(actual).
+
+aumentarContador:-
+    ActualVar is contador(actual),
+/**
+*
+*Funcion encargada de ingresar el nombre de la personar que esta jugando
+*Entradas: Un string
+ */
+guardarNombreJugador(NombreJugador):-
+    asserta(NombreJugador(NombreJugador)).
+
+
+
+
+
+/**
+*Funcion de leer y sis aux
+ */
+%Funcion encargada de leer archivos de texto.
+readFile_Caller(Lines):-
+    open('C:/Users/Usuario/Desktop/pry03/myFile.txt', read, Str),
+    read_file(Str,Lines),
+    close(Str).
+    %write(Lines), nl; 
+    
+%Funcion de apoyo a la funncion de readFile_Caller.
+read_file(Stream,[]) :-
+    at_end_of_stream(Stream).
+%Funcion de apoyo a la funncion de readFile_Caller.
+read_file(Stream,[X|L]) :-
+    \+ at_end_of_stream(Stream),
+    read(Stream,X),
+    read_file(Stream,L).
+
+
+%Funcion encargada de encontrar el elemento que se esta solicitando en la lista y guardarlo.
+evaluarElementoEnposicion(Fila,Columna,Matriz,Movimiento):-
+nth0(Columnas,Matriz,X),
+nth0(Filas,X,Y).
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
 *
 *Funcion encargada de evaluar el caso y el movimiento especifico es un movimiento legal que no sea inter
 *Entradas:Dos strings
