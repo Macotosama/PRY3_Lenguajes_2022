329d7d974e732e4748af6dc447d2f467d0000118
Macotosama <jtn1999@outlook.com>
2022-10-30 19:30:54 -0600

    #

--- laberinto.pl
+++ laberinto.pl
@@ -9,18 +9,18 @@ de un alberinto con los datos necesarios para su finalizacion
 */
 
 
-accionBasadaEnEstado(inter, up).
-accionBasadaEnEstado(inter, down).
-accionBasadaEnEstado(inter, left).
-accionBasadaEnEstado(inter, right).
+accionBasadaEnEstado('up', 'inter').
+accionBasadaEnEstado('down', 'inter').
+accionBasadaEnEstado('left', 'inter').
+accionBasadaEnEstado('right', 'inter').
 
-accionBasadaEnEstado(up, ar).
+accionBasadaEnEstado('up', 'ar').
 
-accionBasadaEnEstado(right, ad).
+accionBasadaEnEstado('right', 'ad').
 
-accionBasadaEnEstado(down, ab).
+accionBasadaEnEstado('down', 'ab').
 
-accionBasadaEnEstado(left, at).
+accionBasadaEnEstado('left', 'at').
 
 
 /**
@@ -29,8 +29,19 @@ accionBasadaEnEstado(left, at).
 *Entradas:Dos strings
  */
 
-evaluarCaso(Y,Movimiento,RespuestaMovimiento):-
-    accionBasadaEnEstado(Y, Movimiento).%->RespuestaMovimiento is 1,
+ %evaluarCaso('inter', Movimiento):- accionBasadaEnEstado(Movimiento, 'inter').
+ %evaluarCaso('ar', Movimiento):- accionBasadaEnEstado(Movimiento, 'ar').
+ %evaluarCaso('ad', Movimiento):- accionBasadaEnEstado(Movimiento, 'ad').
+ %evaluarCaso('ab', Movimiento):- accionBasadaEnEstado(Movimiento, 'ab').
+ %evaluarCaso('at', Movimiento):- accionBasadaEnEstado(Movimiento, 'at').
+
+evaluarCaso(Y,Movimiento):-
+    (Y='inter')->accionBasadaEnEstado(Movimiento, 'inter');
+    (Y='ar')->accionBasadaEnEstado(Movimiento, 'ar');
+    (Y='ad')->accionBasadaEnEstado(Movimiento, 'ad');
+    (Y='ab')->accionBasadaEnEstado(Movimiento, 'ab');
+    (Y='at')->accionBasadaEnEstado(Movimiento, 'at').
+    %accionBasadaEnEstado(Y, Movimiento).%->RespuestaMovimiento is 1,
     %RespuestaMovimiento is 0.
 
 /**
@@ -38,9 +49,9 @@ evaluarCaso(Y,Movimiento,RespuestaMovimiento):-
  * Entradas: Filas, columnas, una matriz, el comando de movimiento(debe concidir con los siguientes: ar, ad, ab, at, inter)
 
  */
-optenerElmentoEnPosicion(Fila,Columna,Matriz,Movimiento,RespuestaMovimiento):-
+optenerElmentoEnPosicion(Fila,Columna,Matriz,Movimiento):-
     nth0(Columnas,Matriz,X),
     nth0(Filas,X,Y),
-    evaluarCaso(Y,Movimiento,RespuestaMovimiento).
+    evaluarCaso(Y,Movimiento).
 %Se debe evaluar que el movimiento resultante y el que se deseo ingresar sean correspondientes. 
  
