1d42dcb0bcc012f0287709301ddbba0184158c95
tim-scarlith <anthony00052023@gmail.com>
2022-11-01 18:16:56 -0600

    commit

--- laberinto.pl
+++ laberinto.pl
@@ -11,25 +11,43 @@ de un alberinto con los datos necesarios para su finalizacion
 
 
 estadoGanador(f).
-
+/**
 accionBasadaEnEstado(inter, up).
 accionBasadaEnEstado(inter, down).
 accionBasadaEnEstado(inter, left).
 accionBasadaEnEstado(inter, right).
+accionBasadaEnEstado(right, inter).
+accionBasadaEnEstado(up, ar).
+accionBasadaEnEstado(right, ad).
+accionBasadaEnEstado(down, ab).
+accionBasadaEnEstado(left, at).*/
 
+
+
+accionBasadaEnEstado(right, right).
 accionBasadaEnEstado(right, inter).
 
+accionBasadaEnEstado(down, down).
+accionBasadaEnEstado(down, inter).
 
+accionBasadaEnEstado(up, up).
+accionBasadaEnEstado(up, inter).
 
-accionBasadaEnEstado(up, ar).
+accionBasadaEnEstado(left, inter).
+accionBasadaEnEstado(left, left).
 
+accionBasadaEnEstado(inter, inter).
+accionBasadaEnEstado(inter, left).
+accionBasadaEnEstado(inter, down).
+accionBasadaEnEstado(inter, up).
+accionBasadaEnEstado(inter, right).
+accionBasadaEnEstado(inter, f).
 
-accionBasadaEnEstado(right, ad).
-
-accionBasadaEnEstado(down, ab).
-
-accionBasadaEnEstado(left, at).
-
+accionBasadaEnEstado(i, inter).
+accionBasadaEnEstado(i, left).
+accionBasadaEnEstado(i, down).
+accionBasadaEnEstado(i, up).
+accionBasadaEnEstado(i, right).
 
 obtenerMatriz(X):-
     globalMatriz(X).
@@ -39,20 +57,20 @@ obtenerMatriz(X):-
  */
 %Funcion encargada de leer archivos de texto.
 readFile_Caller(Dirrecion):-
-    open('D:/trabajis/Lenguajes/proyecto3/myFile.txt', read, Str),
-    read_file(Str,Lines),
+    %open('D:/trabajis/Lenguajes/proyecto3/myFile.txt', read, Str),
+    open('C:/Users/Usuario/Desktop/pry03/PRY3_Lenguajes_2022/myFile.txt', read, Str),
+    read_file(Str,Lines, _),
     close(Str),
-    asserta(globalMatriz(Lines)).
+    asserta(globalMatriz(Lines)), buscarInicio.
     %write(Lines), nl; 
     
 %Funcion de apoyo a la funncion de readFile_Caller.
-read_file(Stream,[]) :-
-    at_end_of_stream(Stream).
+read_file(_,[], R) :-
+    R == end_of_file, !.
 %Funcion de apoyo a la funncion de readFile_Caller.
-read_file(Stream,[X|L]) :-
-    \+ at_end_of_stream(Stream),
+read_file(Stream,[X|L], R) :-
     read(Stream,X),
-    read_file(Stream,L).
+    read_file(Stream,L, X).
 
 
 %Funcion encargada de encontrar el elemento que se esta solicitando en la lista y guardarlo.
@@ -64,14 +82,80 @@ nth0(Filas,X,Y).
 
 
 /**
+*Funcion encargada de buscar la i o elemento inical
+*Entradas:Na
+*/
+buscarInicio:-
+    globalMatriz([A|B]),
+    member(i,A),
+    I is 0,
+    J is 0,
+    buscarEnColumnas(I,J,A),!.
+
+/**
+*Funcion encargada de buscar la i o elemento inical
+*Entradas:Na
+*/
+buscarInicio:-
+    globalMatriz([A|B]),
+    not(member(i,A)),
+    I is 1,
+    J is 0,
+    buscarEnFilas(I,J,B).
+
+buscarInicio.
+/**
+*Funcion auxiliar de buscar la buscarInicio o elemento iniciando desde las filas
+*Entradas:Na
+*/
+buscarEnFilas(I,J,[A|B]):-
+    member(i,A),
+    buscarEnColumnas(I,J,A).
+/**
+*Funcion auxiliar de buscar la buscarInicio o elemento iniciando desde las filas
+*Entradas:Na
+*/
+buscarEnFilas(I,J,[A|B]):-
+    not(member(i,A)),
+    R is I+1,
+    buscarEnFilas(R,J,B).
+buscarEnFilas(_,_,[]).
+/**
+*Funcion auxiliar de buscar la buscarInicio o elemento iniciando desde las columnas
+*Entradas:Na
+*/
+buscarEnColumnas(I,J,[A|B]) :-
+    A == i,
+    write("tramado"),
+    asserta(posicionActual(I,J)),
+    asserta(datoActual(i)), !.
+/**
+*Funcion auxiliar de buscar la buscarInicio o elemento iniciando desde las columnas
+*Entradas:Na
+*/
+buscarEnColumnas(I,J,[A|B]):-
+    not(A == i),
+    R is J+1,
+    write("sigue"),
+    buscarEnColumnas(I,R,B).
+buscarEnColumnas(_,_,[]).
+
+
+
+
+/**
 *
 *Funcion encargada de evaluar el caso y el movimiento especifico es un movimiento legal que no sea inter
 *Entradas:Na
  */
 evaluarCaso :-
-    datoMovimiento(X), datoPosicion(Y), write(X), write(Y),
-    accionBasadaEnEstado(X, Y).
-    
+    %datoMovimiento(X), datoPosicion(Y), write(X), write(Y),
+    datoActual(X), datoPosicion(Y),
+    accionBasadaEnEstado(X, Y),
+    retractall(datoActual(_)),
+    asserta(datoActual(Y)),
+    retractall(datoPosicion(_)).
+
 /**
 *Funcion encargada de evaluar si ya se llego al punto de gane 
 *Entradas:Na
@@ -83,15 +167,15 @@ evaluargane:-
  * Funcion encargada de encontrar el elemento que se esta solicitado en una lista
  * Entradas: Filas, columnas, una matriz, el comando de movimiento(debe concidir con los siguientes: ar, ad, ab, at, inter)
  */
-optenerElmentoEnPosicion(Filas,Columnas,Movimiento):-
+optenerElmentoEnPosicion(Filas,Columnas):-
     globalMatriz(M),
     nth0(Filas,M,X),
     nth0(Columnas,X,Y),
     retractall(datoPosicion(_)),
-    retractall(datoMovimiento(_)),
+    
 
-    asserta(datoPosicion(Y)),
-    asserta(datoMovimiento(Movimiento)).
+    asserta(datoPosicion(Y)).
+    
 
 /**
 *Función encargada de buscar el camino a la salida del mapa
